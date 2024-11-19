95e5d7ab03603b2844974804bde45aad890a2094
Entity-903 <charlesmassie021@gmail.com>
2024-11-18 22:54:47 -0700

    GLM Integration complete

--- 2D/Source/MathUtils.h
+++ 2D/Source/MathUtils.h
@@ -1,3 +1,5 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #pragma once
 
 template<typename T>
@@ -41,4 +43,10 @@ inline void CubicPoint(int x1, int y1, int x2, int y2, int x3, int y3, int x4, i
 
 	x = (int)(a * x1 + b * x2 + c * x3 + d * x4);
 	y = (int)(a * y1 + b * y2 + c * y3 + d * y4);
+}
+
+inline void TransformMatrix()
+{
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::translate(model, glm::vec3(1.0f, 0.0f, 0.0f));
 }
